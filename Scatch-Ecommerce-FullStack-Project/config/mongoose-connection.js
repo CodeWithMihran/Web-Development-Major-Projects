292dd39879430af77175cc793f40d3a110a2eb73
CodeWithMihran <sohail.mihran@gmail.com>
2026-02-28 23:42:18 +0530

    Created most of the project

--- Scatch-Ecommerce-FullStack-Project/config/mongoose-connection.js
+++ Scatch-Ecommerce-FullStack-Project/config/mongoose-connection.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
+const config = require('config');
 
-mongoose.connect("mongodb://127.0.0.1:27017/scatch")
+const dbgr = require('debug')("development:mongoose");
+
+mongoose.connect(`${config.get("MONGODB_URI")}/scatch`)
 
 .then(function(){
-    console.log("Connected");
+    dbgr("Connected");
 })
 
 .catch(function(err){
-    console.log(err);
+    dbgr(err);
 })
 
 module.exports = mongoose.connection;
--- Scatch-Ecommerce-FullStack-Project/routes/usersRouter.js
+++ Scatch-Ecommerce-FullStack-Project/routes/usersRouter.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const isloggedin = require("../middlewares/isLoggedIn");
+const {registerUser, loginUser, logout} = require("../controllers/authController");
 
-router.get('/', function(req, res){
+router.get('/', function (req, res) {
     res.send("Hey it's working");
 })
+
+router.post('/register', registerUser);
+
+router.post('/login', loginUser);
+
+router.post('/logout', logout);
 
 module.exports = router;
