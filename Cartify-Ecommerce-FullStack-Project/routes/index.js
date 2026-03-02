30cc210f14b10f5aa3b4f045ea521f62fc1bb635
CodeWithMihran <sohail.mihran@gmail.com>
2026-03-02 21:51:01 +0530

    Some updates in the project

---
+++ Cartify-Ecommerce-FullStack-Project/routes/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const isLoggedIn = require("../middlewares/isLoggedIn");
+const productModel = require("../models/product-model");
+const userModel = require("../models/user-model");
+
+router.get('/', function (req, res) {
+    let error = req.flash("error");
+    res.render("index", { error, loggedin: false });
+})
+
+router.get("/shop", isLoggedIn, async function (req, res) {
+    let products = await productModel.find();
+    let success = req.flash("success");
+    res.render("shop", { products, success });
+});
+
+router.get("/cart", isLoggedIn, async function (req, res) {
+    let user = await userModel
+        .findOne({ email: req.user.email })
+        .populate("cart");
+
+    let cartTotal = 0;
+
+    user.cart.forEach(item => {
+        cartTotal += Number(item.price) - Number(item.discount);
+    });
+
+    cartTotal += 20;
+
+    res.render("cart", { user, cartTotal });
+});
+
+router.get("/addtocart/:productid", isLoggedIn, async function (req, res) {
+    let user = await userModel.findOne({ email: req.user.email });
+    user.cart.push(req.params.productid);
+    await user.save();
+    req.flash("success", "Added to cart");
+    res.redirect("/shop");
+});
+
+router.get("/logout", function(req, res) {
+    res.cookie("token", "");
+    req.flash("success", "Logged In successfully");
+    res.redirect("/");
+});
+
+module.exports = router;
