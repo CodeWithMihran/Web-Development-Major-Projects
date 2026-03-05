f8b997f588a1a7322378992d8939e4034493f98d
CodeWithMihran <sohail.mihran@gmail.com>
2026-03-05 23:43:57 +0530

    Some Deployment Changes

--- app.js
+++ app.js
@@ -3,26 +3,27 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 const path = require('path');
+
 const indexRouter = require("./routes/index");
 const ownersRouter = require("./routes/ownersRouter");
 const usersRouter = require("./routes/usersRouter");
 const productsRouter = require("./routes/productsRouter");
-const env = require('dotenv');
+
 const expressSession = require('express-session');
 const flash = require('connect-flash');
 
 const db = require("./config/mongoose-connection");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(expressSession({
     resave: false,
     saveUninitialized: false,
     secret: process.env.EXPRESS_SESSION_SECRET,
-})
-);
+}));
+
 app.use(flash());
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -33,4 +34,8 @@ app.use('/owners', ownersRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
